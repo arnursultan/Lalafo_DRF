303225e8a1dc147886e8003727cdc4af5dc89557
Nursultan Zhanybekov <121675952+arnursultan@users.noreply.github.com>
2023-05-18 21:49:37 +0600

    6 Lesson

--- apps/posts/urls.py
+++ apps/posts/urls.py
@@ -1,15 +1,10 @@
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from django.urls import path
-from apps.posts.views import PostAPIViewSet
+
+from apps.posts.views import PostAPIViewSet, FavoritePostAPIViewSet
 
 
 router = DefaultRouter()
 router.register('posts',  PostAPIViewSet, "api_posts")
+router.register('favorite', FavoritePostAPIViewSet, "api_favorite_posts")
 
-urlpatterns = [
-    path('login/', TokenObtainPairView.as_view(), name="api_login"),
-    path('login/refresh', TokenRefreshView.as_view(), name="api_token_refresh"),
-]
-
-urlpatterns += router.urls
+urlpatterns = router.urls
